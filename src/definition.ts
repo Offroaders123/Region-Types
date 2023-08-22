767088e319f5c1d380f4738c8d36d99891ecc690
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-08-21 22:34:11 -0700

    Dependency Upgrades (NBTify + DoubleTime Studios)
    'The Mistakes' is a great album so far! Already loving it! Listening to it right now.

--- src/definition.ts
+++ src/definition.ts
@@ -1,6 +1,6 @@
-import { NBTData, TAG, getTagType, fromListUnsafe, fromCompoundUnsafe } from "nbtify";
+import { NBTData, TAG, isTag, getTagType } from "nbtify";
 
-import type { RootTag, Tag, ListTag, ListTagUnsafe, CompoundTag, CompoundTagUnsafe } from "nbtify";
+import type { Tag, ListTag, CompoundTag } from "nbtify";
 
 export interface DefinitionOptions {
   name: string;
@@ -9,10 +9,10 @@ export interface DefinitionOptions {
 /**
  * Generates a TypeScript interface definition from an NBTData object.
 */
-export function definition(data: RootTag | NBTData, options: DefinitionOptions): string;
-export function definition(data: RootTag | NBTData, { name }: DefinitionOptions): string {
+export function definition<T extends CompoundTag>(data: T | NBTData<T>, options: DefinitionOptions): string;
+export function definition<T extends CompoundTag>(data: T | NBTData<T>, { name }: DefinitionOptions): string {
   if (data instanceof NBTData){
-    data = data.data as CompoundTagUnsafe;
+    data = data.data;
   }
 
   if (typeof data !== "object" || data === null){
@@ -39,10 +39,10 @@ export class DefinitionWriter {
   /**
    * Initiates the writer over an NBTData object.
   */
-  write(data: RootTag | NBTData, options: DefinitionWriterOptions): string;
-  write(data: RootTag | NBTData, { name }: DefinitionWriterOptions): string {
+  write<T extends CompoundTag>(data: T | NBTData<T>, options: DefinitionWriterOptions): string;
+  write<T extends CompoundTag>(data: T | NBTData<T>, { name }: DefinitionWriterOptions): string {
     if (data instanceof NBTData){
-      data = data.data as CompoundTagUnsafe;
+      data = data.data;
     }
 
     if (typeof data !== "object" || data === null){
@@ -55,7 +55,7 @@ export class DefinitionWriter {
     this.#space = "  ";
     this.#level = 1;
 
-    return `interface ${name} ${this.#writeCompound(fromCompoundUnsafe(data as CompoundTagUnsafe))}`;
+    return `interface ${name} ${this.#writeCompound(data)}`;
   }
 
   #writeTag(value: Tag): string {
@@ -69,8 +69,8 @@ export class DefinitionWriter {
       case TAG.DOUBLE: return "DoubleTag";
       case TAG.BYTE_ARRAY: return "ByteArrayTag";
       case TAG.STRING: return "StringTag";
-      case TAG.LIST: return this.#writeList(fromListUnsafe(value as ListTagUnsafe));
-      case TAG.COMPOUND: return this.#writeCompound(fromCompoundUnsafe(value as CompoundTagUnsafe));
+      case TAG.LIST: return this.#writeList(value as ListTag<Tag>);
+      case TAG.COMPOUND: return this.#writeCompound(value as CompoundTag);
       case TAG.INT_ARRAY: return "IntArrayTag";
       case TAG.LONG_ARRAY: return "LongArrayTag";
       default: throw new Error(`Encountered unsupported tag type '${type}'`);
@@ -83,9 +83,10 @@ export class DefinitionWriter {
     return (singleQuoteString.length < doubleQuoteString.length) ? `'${singleQuoteString}'` : `"${doubleQuoteString}"`;
   }
 
-  #writeList(value: ListTag): string {
+  #writeList(value: ListTag<Tag>): string {
+    value = value.filter(isTag);
     const fancy = (this.#space !== "");
-    const type = (value.length !== 0) ? getTagType(value[0])! : TAG.END;
+    const type: TAG = (value[0] !== undefined) ? getTagType(value[0]) : TAG.END;
     const isIndentedList = fancy && new Set<TAG>([TAG.BYTE_ARRAY,TAG.LIST,TAG.COMPOUND,TAG.INT_ARRAY,TAG.LONG_ARRAY]).has(type);
     return `[${value.map(entry => `${isIndentedList ? `\n${this.#space.repeat(this.#level)}` : ""}${(() => {
       this.#level += 1;
@@ -97,7 +98,7 @@ export class DefinitionWriter {
 
   #writeCompound(value: CompoundTag): string {
     const fancy = (this.#space !== "");
-    return `{${Object.entries(value).map(([key,value]) => `${fancy ? `\n${(this.#space satisfies string).repeat(this.#level)}` : ""}${/^[0-9a-z_\-.+]+$/i.test(key) ? key : this.#writeStringLiteral(key)}:${fancy ? " " : ""}${(() => {
+    return `{${Object.entries(value).filter((entry): entry is [string,Tag] => isTag(entry[1])).map(([key,value]) => `${fancy ? `\n${(this.#space satisfies string).repeat(this.#level)}` : ""}${/^[0-9a-z_\-.+]+$/i.test(key) ? key : this.#writeStringLiteral(key)}:${fancy ? " " : ""}${(() => {
       this.#level += 1;
       const result = `${this.#writeTag(value)};`;
       this.#level -= 1;

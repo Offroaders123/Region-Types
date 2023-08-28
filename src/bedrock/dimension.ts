63161bad6ade23ee9aec74374058a936f9f9fd2f
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-08-28 13:48:58 -0700

    Dimension IDs
    Recently rediscovered this repo, and it had some missing information about Dimension IDs for Bedrock. Added those, and got the IDs for Java from in-game in the F3 menu while in each dimension.
    
    https://github.com/Mojang/bedrock-samples
    
    I think it might make sense to organize the files/modules into names that correspond to the Resource Locations that Java organizes it's code into. Or, something similar. I think I'm kind of tending to use these structures for things implicitly already.
    
    https://minecraft.fandom.com/wiki/Resource_location

---
+++ src/bedrock/dimension.ts
@@ -0,0 +1,5 @@
+export enum DimensionResource {
+  overworld = "minecraft:overworld",
+  nether = "minecraft:nether",
+  the_end = "minecraft:the_end"
+}
---
+++ src/java/dimension.ts
@@ -0,0 +1,5 @@
+export enum DimensionResource {
+  overworld = "minecraft:overworld",
+  the_nether = "minecraft:the_nether",
+  the_end = "minecraft:the_end"
+}

d41f08f2fde41446d22dfd416a09c58b40414bbf
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-08-28 22:47:57 -0700

    Complete Java Block Entities
    Wow! On a roll the last few days. I think I'm in one of those time when thing I've been contemplating have been clicking. Great for coding, and music too.
    
    This should complete the documentation for all of the Block Entities present in Java Edition! There's still a few missing spots where the wiki didn't have all of the information, but I think I've covered all of the features in general. Some just might need some help with accuracy or undocumented types. Next round will be Bedrock! Not sure if I'm gonna want to do that tomorrow or whatever, and I'm not sure why I wanted to do so many of these already in general, that was interesting for sure.
    
    https://minecraft.fandom.com/wiki/Chunk_format#Block_entity_format
    
    How many hours did I code today? Too many? Never! I guess I'll do some music recording before I go to bed. Really nice to have a day off today, had a 40 hour work week this week, maybe that's why I felt good to concentrate on this one today. Didn't get a run in, but that's ok because I can get more in this week, I have more time for some. It was hot today too, eek. Always good to get bonus rest after harder runs too, which I got to do last week.

--- src/java/block-entity.ts
+++ src/java/block-entity.ts
@@ -1,9 +1,10 @@
-import type { BooleanTag, ByteTag, ShortTag, IntTag, LongTag, StringTag, IntArrayTag } from "nbtify";
+import type { BooleanTag, ByteTag, ShortTag, IntTag, LongTag, FloatTag, StringTag, IntArrayTag } from "nbtify";
+import type { BlockState, BlockResource } from "./block.js";
 import type { EffectID } from "./effect.js";
 import type { Entity } from "./entity.js";
 import type { Item } from "./item.js";
 
-export type BlockEntity = Banner | Barrel | Beacon | Beehive | BlastFurnace | BrewingStand | BrushableBlock | CalibratedSculkSensor | Campfire;
+export type BlockEntity = Banner | Barrel | Beacon | Beehive | BlastFurnace | BrewingStand | BrushableBlock | CalibratedSculkSensor | Campfire | Chest | ChiseledBookshelf | Comparator | CommandBlock | Conduit | Dispenser | Dropper | EnchantingTable | EndGateway | Furnace | HangingSign | Hopper | Jigsaw | Jukebox | Lectern | MobSpawner | Piston | SculkCatalyst | SculkSensor | SculkShrieker | ShulkerBox | Sign | Skull | Smoker | SoulCampfire | StructureBlock | TrappedChest;
 
 export interface Banner extends BlockEntityLike {
   id: typeof BlockEntityResource.banner;
@@ -66,13 +67,13 @@ export interface BlastFurnace extends BlockEntityLike {
   CookTime: ShortTag;
   CookTimeTotal: ShortTag;
   CustomName?: StringTag;
-  Items: Item[];
+  Items: Item[]; // Like `Campfire`, this should really be `[Item,Item,Item]`.
   Lock?: StringTag;
   RecipesUsed: BlastFurnaceRecipesUsed;
 }
 
 export interface BlastFurnaceRecipesUsed {
-  [id: number]: IntTag; // Use `RecipeID` for the index once that is implemented.
+  [id: number]: IntTag; // Use `BlastFurnaceRecipeID` for the index once that is implemented.
 }
 
 export interface BrewingStand extends BlockEntityLike {
@@ -104,6 +105,342 @@ export interface Campfire extends BlockEntityLike {
   CookingTotalTimes: IntArrayTag;
   Items: Item[]; // Should be `[Item,Item,Item,Item]`, but I want the values to be optional. They're not always present there!
 }
+
+export interface Chest extends BlockEntityLike {
+  id: typeof BlockEntityResource.chest;
+  CustomName?: StringTag;
+  Items: Item[];
+  Lock?: StringTag;
+  LootTable?: StringTag; // `LootTableResource`
+  LootTableSeed?: LongTag;
+}
+
+export interface ChiseledBookshelf extends BlockEntityLike {
+  id: typeof BlockEntityResource.chiseled_bookshelf;
+  Items: Item[];
+  last_interacted_slot: IntTag<ChiseledBookshelfLastInteractedSlot>;
+}
+
+export type ChiseledBookshelfLastInteractedSlot = -1 | 0 | 1 | 2 | 3 | 4 | 5;
+
+export interface Comparator extends BlockEntityLike {
+  id: typeof BlockEntityResource.comparator;
+  OutputSignal: IntTag;
+}
+
+export interface CommandBlock extends BlockEntityLike {
+  id: typeof BlockEntityResource.command_block;
+  auto: BooleanTag;
+  Command: StringTag;
+  conditionMet: BooleanTag;
+  CustomName?: StringTag;
+  LastExecution: LongTag;
+  LastOutput: StringTag;
+  powered: BooleanTag;
+  SuccessCount: IntTag;
+  TrackOutput: BooleanTag;
+  UpdateLastExecution: BooleanTag;
+}
+
+export interface Conduit extends BlockEntityLike {
+  id: typeof BlockEntityResource.conduit;
+  Target?: IntArrayTag;
+}
+
+export interface Dispenser extends BlockEntityLike {
+  id: typeof BlockEntityResource.dispenser;
+  CustomName?: StringTag;
+  Items: Item[];
+  Lock?: StringTag;
+  LootTable?: StringTag; // `LootTableResource`
+  LootTableSeed?: LongTag;
+}
+
+export interface Dropper extends BlockEntityLike {
+  id: typeof BlockEntityResource.dropper;
+  CustomName?: StringTag;
+  Items: Item[];
+  Lock?: StringTag;
+  LootTable?: StringTag; // `LootTableResource`
+  LootTableSeed?: LongTag;
+}
+
+export interface EnchantingTable extends BlockEntityLike {
+  id: typeof BlockEntityResource.enchanting_table;
+  CustomName?: StringTag;
+}
+
+export interface EndGateway extends BlockEntityLike {
+  id: typeof BlockEntityResource.end_gateway;
+  Age: LongTag;
+  ExactTeleport: BooleanTag;
+  ExitPortal: EndGatewayExitPortal;
+}
+
+export type EndGatewayExitPortal = [IntTag,IntTag,IntTag];
+
+export interface Furnace extends BlockEntityLike {
+  id: typeof BlockEntityResource.furnace;
+  BurnTime: ShortTag;
+  CookTime: ShortTag;
+  CookTimeTotal: ShortTag;
+  CustomName?: StringTag;
+  Items: Item[]; // `[Item,Item,Item]`
+  Lock?: StringTag;
+  RecipesUsed: FurnaceRecipesUsed;
+}
+
+export interface FurnaceRecipesUsed {
+  [id: number]: IntTag; // Use `FurnaceRecipeID` for the index once that is implemented.
+}
+
+export interface HangingSign extends SignLike {
+  id: typeof BlockEntityResource.hanging_sign;
+}
+
+export interface Hopper extends BlockEntityLike {
+  id: typeof BlockEntityResource.hopper;
+  CustomName?: StringTag;
+  Items: Item[];
+  Lock?: StringTag;
+  LootTable?: StringTag; // `LootTableResource`
+  LootTableSeed?: LongTag;
+  TransferCooldown: IntTag<HopperTransferCooldown>;
+}
+
+export type HopperTransferCooldown = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+
+export interface Jigsaw extends BlockEntityLike {
+  id: typeof BlockEntityResource.jigsaw;
+  final_state: BlockResource;
+  joint: JigsawJoint;
+  name: StringTag;
+  pool: StringTag;
+  target: StringTag;
+}
+
+export type JigsawJoint = "rollable" | "aligned";
+
+export interface Jukebox extends BlockEntityLike {
+  id: typeof BlockEntityResource.jukebox;
+  IsPlaying: BooleanTag;
+  RecordItem: Item;
+  RecordStartTick: LongTag;
+  TickCount: LongTag;
+}
+
+export interface Lectern extends BlockEntityLike {
+  id: typeof BlockEntityResource.lectern;
+  Book?: Item;
+  Page: IntTag;
+}
+
+// Double check that some of these properties are mandatory; the wiki doesn't quite specify, but it make it sound like they could be optional.
+export interface MobSpawner extends BlockEntityLike {
+  id: typeof BlockEntityResource.mob_spawner;
+  Delay: ShortTag;
+  MaxNearbyEntities: ShortTag;
+  MaxSpawnDelay: ShortTag;
+  MinSpawnDelay: ShortTag;
+  RequiredPlayerRange: ShortTag;
+  SpawnCount: ShortTag;
+  SpawnData: Entity; // Also not sure what this should really be, other than a template of properties to add to your supplied mob type? I think?
+  SpawnPotentials?: MobSpawnerSpawnPotential[];
+  SpawnRange: ShortTag;
+}
+
+export interface MobSpawnerSpawnPotential {
+  weight: IntTag;
+  data: Entity; // Related to `SpawnData`, looks like it will override the properties from that one, if this is present instead.
+  customSpawnRules: MobSpawnerSpawnRules;
+}
+
+export interface MobSpawnerSpawnRules {
+  block_light_limit: MobSpawnerLightLimit;
+  sky_light_limit: MobSpawnerLightLimit;
+}
+
+export type MobSpawnerLightLimit = IntTag | {
+  min_inclusive: IntTag;
+  max_inclusive: IntTag;
+};
+
+export interface Piston extends BlockEntityLike {
+  id: typeof BlockEntityResource.piston;
+  blockState: BlockState; // I think this is just the raw `BlockState` type, not totally sure though? Might be a wrapper? I don't think so though.
+  extending: BooleanTag;
+  facing: IntTag<PistonFacing>;
+  progress: FloatTag;
+  source: BooleanTag;
+}
+
+export type PistonFacing = 0 | 1 | 2 | 3 | 4 | 5;
+
+export interface SculkCatalyst extends BlockEntityLike {
+  id: typeof BlockEntityResource.sculk_catalyst;
+  charges: SculkCatalystCharge[];
+}
+
+export interface SculkCatalystCharge {
+  charge: IntTag;
+  pos: SculkCatalystChargePosition;
+  /**
+   * Does not relate to delay.
+   * 
+   * Be 1 if the charge was spread from a sculk or sculk vein, 0 otherwise.
+   * The charge can spread to any block if this tag is 1.
+   * If it is 0, all the powers in the charge disappear when it spreads to a block not in sculk family.
+  */
+  decay_delay: SculkCatalystChargeSpread;
+  update_delay: IntTag;
+  facings: SculkCatalystFacings;
+}
+
+export type SculkCatalystChargePosition = [IntTag,IntTag,IntTag];
+
+export type SculkCatalystChargeSpread = IntTag<0 | 1>;
+
+// This is probably wrong, the wiki wasn't too clear to me.
+export type SculkCatalystFacings = [SculkCatalystFacing,SculkCatalystFacing,SculkCatalystFacing,SculkCatalystFacing,SculkCatalystFacing,SculkCatalystFacing];
+
+export type SculkCatalystFacing = "north" | "south" | "east" | "west" | "up" | "down";
+
+export interface SculkSensor extends BlockEntityLike {
+  id: typeof BlockEntityResource.sculk_sensor;
+  // Can't find documentation about the Block Data here
+}
+
+export interface SculkShrieker extends BlockEntityLike {
+  id: typeof BlockEntityResource.sculk_shrieker;
+  // Can't find documentation about the Block Data here
+}
+
+export interface ShulkerBox extends BlockEntityLike {
+  id: typeof BlockEntityResource.shulker_box;
+  CustomName?: StringTag;
+  Items: Item[];
+  Lock?: StringTag;
+  LootTable?: StringTag; // `LootTableResource`
+  LootTableSeed?: LongTag;
+}
+
+export interface Sign extends SignLike {
+  id: typeof BlockEntityResource.sign;
+}
+
+export interface Skull extends BlockEntityLike {
+  id: typeof BlockEntityResource.skull;
+  note_block_sound?: StringTag; // "The sound event this skull plays when played with a note block." probably should be typed to a Resource Location :)
+  ExtraType: StringTag;
+  SkullOwner?: SkullOwner;
+}
+
+export interface SkullOwner {
+  Id?: IntArrayTag;
+  Name?: StringTag;
+  Properties: [SkullTexture];
+}
+
+export interface SkullTexture {
+  /**
+   * A Base64-encoded JSON object. Equivalent to the type SkullTextureJSON.
+  */
+  Value: StringTag;
+  Signature?: StringTag;
+}
+
+export interface SkullTextureJSON {
+  isPublic?: boolean;
+  signatureRequired: boolean;
+  profileId?: string;
+  textures: {
+    CAPE?: {
+      url: string;
+    };
+    SKIN: {
+      url: string;
+      metadata: {
+        model: "classic" | "slim";
+      };
+    };
+  };
+  /**
+   * LongTag (BigInt), but not really because it's from JSON.
+  */
+  timestamp: number;
+}
+
+export interface Smoker extends BlockEntityLike {
+  id: typeof BlockEntityResource.smoker;
+  BurnTime: ShortTag;
+  CookTime: ShortTag;
+  CookTimeTotal: ShortTag;
+  CustomName?: StringTag;
+  Items: Item[]; // `[Item,Item,Item]`
+  Lock?: StringTag;
+  RecipesUsed: SmokerRecipesUsed;
+}
+
+export interface SmokerRecipesUsed {
+  [id: number]: IntTag; // Use `SmokerRecipeID` for the index once that is implemented.
+}
+
+export interface SoulCampfire extends BlockEntityLike {
+  id: typeof BlockEntityResource.soul_campfire;
+  CookingTimes: IntArrayTag;
+  CookingTotalTimes: IntArrayTag;
+  Items: Item[]; // Like `Campfire`, this should really be `[Item,Item,Item]`.
+}
+
+export interface StructureBlock extends BlockEntityLike {
+  id: typeof BlockEntityResource.structure_block;
+  author: StringTag;
+  ignoreEntities: BooleanTag;
+  integrity: FloatTag;
+  metadata: StringTag;
+  mirror: StructureBlockMirror;
+  mode: StructureBlockMode;
+  name: StringTag;
+  posX: IntTag;
+  posY: IntTag;
+  posZ: IntTag;
+  powered: BooleanTag;
+  rotation: StructureBlockRotation;
+  seed: LongTag;
+  showboundingbox: BooleanTag;
+  sizeX: IntTag;
+  sizeY: IntTag;
+  sizeZ: IntTag;
+}
+
+export type StructureBlockMirror = "NONE" | "LEFT_RIGHT" | "FRONT_BACK";
+
+export type StructureBlockMode = "SAVE" | "LOAD" | "CORNER" | "DATA";
+
+export type StructureBlockRotation = "NONE" | "CLOCKWISE_90" | "CLOCKWISE_180" | "COUNTERCLOCKWISE_90";
+
+export interface TrappedChest extends BlockEntityLike {
+  id: typeof BlockEntityResource.trapped_chest;
+  CustomName?: StringTag;
+  Items: Item[];
+  Lock?: StringTag;
+  LootTable?: StringTag; // `LootTableResource`
+  LootTableSeed?: LongTag;
+}
+
+export interface SignLike extends BlockEntityLike {
+  is_waxed: BooleanTag;
+  front_text: SignText;
+  back_text: SignText;
+}
+
+export interface SignText {
+  has_glowing_text: BooleanTag;
+  color: SignColor;
+  messages: StringTag[];
+}
+
+export type SignColor = "black" | "white" | "orange" | "magenta" | "light_blue" | "yellow" | "lime" | "pink" | "gray" | "light_gray" | "cyan" | "purple" | "blue" | "brown" | "green" | "red" | "black";
 
 export interface BlockEntityLike {
   id: BlockEntityResource;
@@ -155,6 +492,7 @@ export const BlockEntityResource = {
   sign: "minecraft:sign",
   skull: "minecraft:skull",
   smoker: "minecraft:smoker",
+  soul_campfire: "minecraft:soul_campfire",
   structure_block: "minecraft:structure_block",
   trapped_chest: "minecraft:trapped_chest"
 } as const;

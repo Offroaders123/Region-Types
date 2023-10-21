e050eff3b648d0081de662314941971f962f58de
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-10-21 10:50:35 -0700

    Bedrock Block Entity IDs
    After some tests with Bedrock-LevelDB, I discovered that Bedrock Block Entities don't use the `minecraft:` ID prefix, unlike Java and LCE (I think LCE too? Need to test that one also). I wasn't sure about the prefix of it initially, but I added them here to start with just in case, and for hopes of it being the most similar. Going to test some more things, to look into more details about Bedrock's level format and it's types.

--- src/bedrock/block-entity.ts
+++ src/bedrock/block-entity.ts
@@ -493,59 +493,59 @@ export interface BlockEntityLike {
 export type BlockEntityResource = typeof BlockEntityResource[keyof typeof BlockEntityResource];
 
 export const BlockEntityResource = {
-  Banner: "minecraft:Banner",
-  Barrel: "minecraft:Barrel",
-  Beacon: "minecraft:Beacon",
-  Bed: "minecraft:Bed",
-  Beehive: "minecraft:Beehive",
-  Bell: "minecraft:Bell",
-  BlastFurnace: "minecraft:BlastFurnace",
-  BrewingStand: "minecraft:BrewingStand",
-  BrushableBlock: "minecraft:BrushableBlock", // Sus
-  CalibratedSculkSensor: "minecraft:CalibratedSculkSensor",
-  Campfire: "minecraft:Campfire",
-  Cauldron: "minecraft:Cauldron",
-  Chalkboard: "minecraft:Chalkboard", // unknown, not on the wiki
-  Chest: "minecraft:Chest",
-  ChiseledBookshelf: "minecraft:ChiseledBookshelf",
-  Comparator: "minecraft:Comparator",
-  CompoundCreator: "minecraft:CompoundCreator", // ID not on the wiki
-  CommandBlock: "minecraft:CommandBlock",
-  Conduit: "minecraft:Conduit",
-  DaylightDetector: "minecraft:DaylightDetector",
-  DecoratedPot: "minecraft:DecoratedPot",
-  Dispenser: "minecraft:Dispenser",
-  Dropper: "minecraft:Dropper",
-  EnchantTable: "minecraft:EnchantTable",
-  EnderChest: "minecraft:EnderChest",
-  EndGateway: "minecraft:EndGateway",
-  EndPortal: "minecraft:EndPortal",
-  ElementConstructor: "minecraft:ElementConstructor", // also guessed
-  FlowerPot: "minecraft:FlowerPot", // ID not listed on the wiki
-  Furnace: "minecraft:Furnace",
-  GlowItemFrame: "minecraft:GlowItemFrame",
-  HangingSign: "minecraft:HangingSign",
-  Hopper: "minecraft:Hopper",
-  ItemFrame: "minecraft:ItemFrame",
-  JigsawBlock: "minecraft:JigsawBlock",
-  Jukebox: "minecraft:Jukebox",
-  LabTable: "minecraft:LabTable", // guessed
-  Lectern: "minecraft:Lectern",
-  Lodestone: "minecraft:Lodestone", // ID not on the wiki
-  MaterialReducer: "minecraft:MaterialReducer", // guessed
-  MobSpawner: "minecraft:MobSpawner",
-  MovingBlock: "minecraft:MovingBlock", // Related to Piston block movement
-  Music: "minecraft:Music", // Note Block
-  NetherReactor: "minecraft:NetherReactor",
-  PistonArm: "minecraft:PistonArm",
-  Poster: "minecraft:Poster", // might be part of Chalkboard?
-  SculkCatalyst: "minecraft:SculkCatalyst",
-  SculkSensor: "minecraft:SculkSensor",
-  SculkShrieker: "minecraft:SculkShrieker",
-  ShulkerBox: "minecraft:ShulkerBox",
-  Sign: "minecraft:Sign",
-  Skull: "minecraft:Skull",
-  Slate: "minecraft:Slate", // also unknown
-  Smoker: "minecraft:Smoker",
-  StructureBlock: "minecraft:StructureBlock"
+  Banner: "Banner",
+  Barrel: "Barrel",
+  Beacon: "Beacon",
+  Bed: "Bed",
+  Beehive: "Beehive",
+  Bell: "Bell",
+  BlastFurnace: "BlastFurnace",
+  BrewingStand: "BrewingStand",
+  BrushableBlock: "BrushableBlock", // Sus
+  CalibratedSculkSensor: "CalibratedSculkSensor",
+  Campfire: "Campfire",
+  Cauldron: "Cauldron",
+  Chalkboard: "Chalkboard", // unknown, not on the wiki
+  Chest: "Chest",
+  ChiseledBookshelf: "ChiseledBookshelf",
+  Comparator: "Comparator",
+  CompoundCreator: "CompoundCreator", // ID not on the wiki
+  CommandBlock: "CommandBlock",
+  Conduit: "Conduit",
+  DaylightDetector: "DaylightDetector",
+  DecoratedPot: "DecoratedPot",
+  Dispenser: "Dispenser",
+  Dropper: "Dropper",
+  EnchantTable: "EnchantTable",
+  EnderChest: "EnderChest",
+  EndGateway: "EndGateway",
+  EndPortal: "EndPortal",
+  ElementConstructor: "ElementConstructor", // also guessed
+  FlowerPot: "FlowerPot", // ID not listed on the wiki
+  Furnace: "Furnace",
+  GlowItemFrame: "GlowItemFrame",
+  HangingSign: "HangingSign",
+  Hopper: "Hopper",
+  ItemFrame: "ItemFrame",
+  JigsawBlock: "JigsawBlock",
+  Jukebox: "Jukebox",
+  LabTable: "LabTable", // guessed
+  Lectern: "Lectern",
+  Lodestone: "Lodestone", // ID not on the wiki
+  MaterialReducer: "MaterialReducer", // guessed
+  MobSpawner: "MobSpawner",
+  MovingBlock: "MovingBlock", // Related to Piston block movement
+  Music: "Music", // Note Block
+  NetherReactor: "NetherReactor",
+  PistonArm: "PistonArm",
+  Poster: "Poster", // might be part of Chalkboard?
+  SculkCatalyst: "SculkCatalyst",
+  SculkSensor: "SculkSensor",
+  SculkShrieker: "SculkShrieker",
+  ShulkerBox: "ShulkerBox",
+  Sign: "Sign",
+  Skull: "Skull",
+  Slate: "Slate", // also unknown
+  Smoker: "Smoker",
+  StructureBlock: "StructureBlock"
 } as const;

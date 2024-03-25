64f27f72f4743380887ff438ac0d073041918b0c
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-03-25 16:15:28 -0700

    Further Name Casing

--- src/bedrock/block.ts
+++ src/bedrock/block.ts
@@ -475,7 +475,7 @@ export interface BlockStateNameMap {
   glass_pane: GlassPane;
   glow_frame: GlowFrame;
   glow_lichen: GlowLichen;
-  glowingobsidian: Glowingobsidian;
+  glowingobsidian: GlowingObsidian;
   glowstone: Glowstone;
   gold_block: GoldBlock;
   gold_ore: GoldOre;
@@ -2600,7 +2600,7 @@ export interface GlowLichen {
   multi_face_direction_bits: IntTag<0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59 | 60 | 61 | 62 | 63>;
 }
 
-export interface Glowingobsidian {}
+export interface GlowingObsidian {}
 
 export interface Glowstone {}
 
@@ -4919,7 +4919,7 @@ export enum BlockResource {
   netherreactor = "minecraft:netherreactor",
   info_update = "minecraft:info_update",
   info_update2 = "minecraft:info_update2",
-  movingBlock = "minecraft:movingBlock",
+  moving_block = "minecraft:moving_block",
   observer = "minecraft:observer",
   structure_block = "minecraft:structure_block",
   hard_glass = "minecraft:hard_glass",
